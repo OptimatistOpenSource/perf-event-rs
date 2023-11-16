c5446bd5c4ec72d7c8346908f9940e43902f6173
Thaumy <thaumy@outlook.com>
2023-11-16 14:55:44 +0800

    add build err msg

--- src/perf_event/mod.rs
+++ src/perf_event/mod.rs
@@ -1,7 +1,21 @@
+use thiserror::Error;
+use crate::infra::result::WrapResult;
 use crate::syscall::bindings::perf_event_attr;
 
 pub struct PerfEvent {
     // TODO
+}
+
+#[derive(Error, Debug)]
+pub enum BuildError {
+    #[error("This option has been set")]
+    DuplicateSet,
+    #[error("PID is invalid: {0}")]
+    InvalidPid(String),
+    #[error("CPU is invalid: {0}")]
+    InvalidCpu(String),
+    #[error("PID and CPU are not set")]
+    PidAndCpuNotSet,
 }
 
 pub struct Builder {
@@ -31,64 +45,77 @@ pub struct Builder {
     pid == -1 and cpu == -1
            This setting is invalid and will return an error.
     */
-    pid: i32,
-    cpu: i32,
+    pid: Option<i32>,
+    cpu: Option<i32>,
 
-    group_fd: i32,
+    group_fd: Option<i32>,
 
-    flags: u64,
+    flags: Option<u64>,
 }
 
 impl Builder {
     pub fn new() -> Self {
         Self {
             attr: Default::default(),
-            pid: -1,
-            cpu: -1,
-            group_fd: -1,
-            flags: 0,
+            pid: None,
+            cpu: None,
+            group_fd: None,
+            flags: None,
         }
     }
 
-    pub fn current_pid(mut self) -> Self {
-        self.pid = 0;
-        self
-    }
-
-    pub fn any_cpu(mut self) -> Self {
-        self.cpu = -1;
-        self
-    }
-
-    pub fn with_pid(mut self, pid: u32) -> Result<Self, ()> {
-        match pid {
-            0 => Err(()),
-            _ if pid > 2 ^ 22 => Err(()),
-            _ if self.pid > 0 => Err(()),
-            _ => {
-                self.pid = pid as i32;
+    pub fn current_pid(mut self) -> Result<Self, BuildError> {
+        match self.pid {
+            None => {
+                self.pid = Some(0);
                 Ok(self)
             }
+            _ => Err(BuildError::DuplicateSet)
         }
     }
 
-    pub fn with_cpu(mut self, cpu: u32) -> Result<Self, ()> {
-        match cpu {
-            0 => Err(()),
-            _ if cpu > i32::MAX as u32 => Err(()),
-            _ if self.cpu > 0 => Err(()),
-            _ => {
-                self.cpu = cpu as i32;
+    pub fn any_cpu(mut self) -> Result<Self, BuildError> {
+        match self.pid {
+            None => {
+                self.pid = Some(-1);
                 Ok(self)
             }
+            _ => Err(BuildError::DuplicateSet)
         }
     }
 
-    pub fn build(self) -> Result<PerfEvent, ()> {
+    pub fn with_pid(mut self, pid: u32) -> Result<Self, BuildError> {
+        match self.pid {
+            None => match pid {
+                0 => BuildError::InvalidPid("PID is 0".to_string()),
+                _ if pid > 2 ^ 22 => BuildError::InvalidPid(format!("PID {} is too big", pid)),
+                _ => {
+                    self.pid = Some(pid as i32);
+                    return Ok(self);
+                }
+            },
+            _ => BuildError::DuplicateSet,
+        }.wrap_err()
+    }
+
+    pub fn with_cpu(mut self, cpu: u32) -> Result<Self, BuildError> {
+        match self.cpu {
+            None => match cpu {
+                _ if cpu > i32::MAX as u32 => BuildError::InvalidCpu(format!("CPU {} is too big", cpu)),
+                _ => {
+                    self.cpu = Some(cpu as i32);
+                    return Ok(self);
+                }
+            },
+            _ => BuildError::DuplicateSet,
+        }.wrap_err()
+    }
+
+    pub fn build(self) -> Result<PerfEvent, BuildError> {
         match self {
             Builder {
-                pid: -1, cpu: -1, ..
-            } => Err(()),
+                pid: None, cpu: None, ..
+            } => Err(BuildError::PidAndCpuNotSet),
             _ => todo!(),
         }
     }
