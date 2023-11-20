93c23689095a63b4ef97a88fda2c16b66ab10d4e
Thaumy <thaumy@outlook.com>
2023-11-20 14:00:35 +0800

    add cpu_clock test

---
+++ src/perf_event/counting/tests/sw/cpu_clock.rs
@@ -0,0 +1,35 @@
+use crate::counting::{Attr, HwEvent, SwEvent};
+use crate::{Builder, EventScope};
+
+fn workload() {
+    for _ in 0..10000000 {
+        std::hint::black_box(0);
+    }
+}
+
+#[test]
+fn test() {
+    let builder = Builder::new().calling_process().any_cpu();
+    let attr = {
+        let event = SwEvent::CpuClock;
+        let scopes = [EventScope::User, EventScope::Host];
+        Attr::new(event, scopes)
+    };
+    let mut counting = builder.build_counting(attr).unwrap();
+
+    let before = counting.get_count().unwrap();
+    dbg!(before);
+    assert_eq!(before, 0);
+    counting.enable().unwrap();
+
+    workload();
+
+    counting.disable().unwrap();
+    let after = counting.get_count().unwrap();
+    dbg!(after);
+    assert!(after > 0);
+    assert_eq!(after, counting.get_count().unwrap());
+
+    counting.enable().unwrap();
+    assert_ne!(after, counting.get_count().unwrap());
+}
