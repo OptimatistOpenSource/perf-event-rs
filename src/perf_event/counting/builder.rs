c8bbacf6ea93e000d5edce8cb1074a946c07a7e5
Thaumy <thaumy@outlook.com>
2023-11-21 14:28:02 +0800

    add build_counting_group

--- src/perf_event/counting/builder.rs
+++ src/perf_event/counting/builder.rs
@@ -1,4 +1,5 @@
-use crate::counting::{Attr, Counting};
+use crate::counting::{Attr, Counting, CountingGroup};
+use crate::infra::result::WrapResult;
 use crate::{BuildError, Builder};
 
 impl Builder {
@@ -17,6 +18,22 @@ impl Builder {
             } => {
                 unsafe { Counting::new(attr, *pid, *cpu, -1, 0) }.map_err(BuildError::SyscallFailed)
             }
+            _ => todo!(),
+        }
+    }
+
+    pub fn build_counting_group(&self) -> Result<CountingGroup, BuildError> {
+        match self {
+            Builder {
+                pid: None,
+                cpu: None,
+                ..
+            } => Err(BuildError::PidAndCpuNotSet),
+            Builder {
+                pid: Some(pid),
+                cpu: Some(cpu),
+                ..
+            } => unsafe { CountingGroup::new(*pid, *cpu) }.wrap_ok(),
             _ => todo!(),
         }
     }
