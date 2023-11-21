787a80a53592f8eaa2ea4ff92e2b0eb339753298
Thaumy <thaumy@outlook.com>
2023-11-21 14:28:50 +0800

    add add_member and new

--- src/perf_event/counting/group.rs
+++ src/perf_event/counting/group.rs
@@ -1,42 +1,78 @@
-use crate::counting::{ioctl_wrapped, Counting};
+use crate::counting::{ioctl_wrapped, Attr, Counting};
 use crate::syscall;
+use libc::pid_t;
 use std::io;
+use std::io::ErrorKind;
+use std::os::fd::AsRawFd;
 
 pub struct CountingGroup {
+    pid: pid_t,
+    cpu: i32,
     members: Vec<Counting>, // members[0] is the group leader, if it exists.
 }
 
 impl CountingGroup {
-    pub fn enable(&self) -> io::Result<()> {
-        let Some(leader) = self.members.get(0) else {
-            todo!()
+    pub(crate) unsafe fn new(pid: pid_t, cpu: i32) -> CountingGroup {
+        Self {
+            pid,
+            cpu,
+            members: vec![],
+        }
+    }
+
+    fn leader(&self) -> Option<&Counting> {
+        self.members.get(0)
+    }
+
+    pub fn add_member<F>(mut self, attr: Attr) -> io::Result<CountingGroup> {
+        match self.leader() {
+            None => {
+                let leader = unsafe { Counting::new(attr, self.pid, self.cpu, -1, 0) }?;
+                self.members.push(leader);
+            }
+            Some(leader) => {
+                let group_fd = leader.file.as_raw_fd();
+                let member = unsafe { Counting::new(attr, self.pid, self.cpu, group_fd, 0) }?;
+                self.members.push(member);
+            }
         };
-        ioctl_wrapped(
-            &leader.file,
-            syscall::bindings::perf_event_ioctls_ENABLE,
-            Some(syscall::bindings::perf_event_ioc_flags_PERF_IOC_FLAG_GROUP),
-        )
+
+        Ok(self)
+    }
+
+    pub fn enable(&self) -> io::Result<()> {
+        if let Some(leader) = self.leader() {
+            ioctl_wrapped(
+                &leader.file,
+                syscall::bindings::perf_event_ioctls_ENABLE,
+                Some(syscall::bindings::perf_event_ioc_flags_PERF_IOC_FLAG_GROUP),
+            )
+        } else {
+            Err(io::Error::new(ErrorKind::Other, "Group has no members"))
+        }
     }
 
     pub fn disable(&self) -> io::Result<()> {
-        let Some(leader) = self.members.get(0) else {
-            todo!()
-        };
-        ioctl_wrapped(
-            &leader.file,
-            syscall::bindings::perf_event_ioctls_DISABLE,
-            Some(syscall::bindings::perf_event_ioc_flags_PERF_IOC_FLAG_GROUP),
-        )
+        if let Some(leader) = self.leader() {
+            ioctl_wrapped(
+                &leader.file,
+                syscall::bindings::perf_event_ioctls_DISABLE,
+                Some(syscall::bindings::perf_event_ioc_flags_PERF_IOC_FLAG_GROUP),
+            )
+        } else {
+            Err(io::Error::new(ErrorKind::Other, "Group has no members"))
+        }
     }
 
     pub fn reset_count(&self) -> io::Result<()> {
-        let Some(leader) = self.members.get(0) else {
-            todo!()
-        };
-        ioctl_wrapped(
-            &leader.file,
-            syscall::bindings::perf_event_ioctls_RESET,
-            Some(syscall::bindings::perf_event_ioc_flags_PERF_IOC_FLAG_GROUP),
-        )
+        if let Some(leader) = self.leader() {
+            ioctl_wrapped(
+                &leader.file,
+                syscall::bindings::perf_event_ioctls_RESET,
+                Some(syscall::bindings::perf_event_ioc_flags_PERF_IOC_FLAG_GROUP),
+            )
+        } else {
+            Err(io::Error::new(ErrorKind::Other, "Group has no members"))
+        }
     }
 }
