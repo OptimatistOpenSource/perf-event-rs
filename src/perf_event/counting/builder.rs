3063a1b44af2504f82424fd2f5f2260195e8a2b1
Thaumy <thaumy@outlook.com>
2023-11-21 13:32:07 +0800

    use ref self for build_counting

--- src/perf_event/counting/builder.rs
+++ src/perf_event/counting/builder.rs
@@ -3,7 +3,7 @@ use crate::{BuildError, Builder};
 
 impl Builder {
     // TODO
-    pub fn build_counting(self, attr: Attr) -> Result<Counting, BuildError> {
+    pub fn build_counting(&self, attr: Attr) -> Result<Counting, BuildError> {
         match self {
             Builder {
                 pid: None,
@@ -14,7 +14,7 @@ impl Builder {
                 pid: Some(pid),
                 cpu: Some(cpu),
                 ..
-            } => unsafe { Counting::new(attr, pid, cpu, -1, 0) }.map_err(BuildError::SyscallFailed),
+            } => unsafe { Counting::new(attr, *pid, *cpu, -1, 0) }.map_err(BuildError::SyscallFailed),
             _ => todo!(),
         }
     }
