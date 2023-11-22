34a30f5236a23e794c2e5d9a84d76b73282db6eb
Thaumy <thaumy@outlook.com>
2023-11-22 10:14:11 +0800

    fix get_ result in group counting

--- src/perf_event/counting/group.rs
+++ src/perf_event/counting/group.rs
@@ -1,14 +1,15 @@
 use crate::counting::{ioctl_wrapped, Attr, Counting};
 use crate::infra::result::WrapResult;
+use crate::infra::vec::VecExt;
 use crate::syscall;
 use libc::pid_t;
 use std::collections::HashMap;
-use std::io;
 use std::io::{ErrorKind, Read};
 use std::os::fd::AsRawFd;
-use std::os::unix::fs::FileExt;
+use std::{io, ptr};
 
 #[repr(C)]
+#[derive(Debug)]
 pub(crate) struct GroupReadFormatValueFollowed {
     pub event_count: u64, // u64 value;
     pub event_id: u64,    // u64 id;
@@ -17,18 +18,21 @@ pub(crate) struct GroupReadFormatValueFollowed {
 }
 
 #[repr(C)]
+#[derive(Debug, Clone)]
 pub(crate) struct GroupReadFormat {
-    pub member_len: u64,   // u64 nr;
+    pub members_len: u64,  // u64 nr;
     pub time_enabled: u64, // u64 time_enabled;
     pub time_running: u64, // u64 time_running;
                            // follows: struct { .. } values[nr];
 }
 
+#[derive(Debug)]
 pub struct GroupCountingMemberResult {
     pub event_count: u64,
     pub event_lost: u64,
 }
 
+#[derive(Debug)]
 pub struct GroupCountingResult {
     pub time_enabled: u64,
     pub time_running: u64,
@@ -75,36 +79,36 @@ impl CountingGroup {
     }
 
     pub fn get_result(&mut self) -> io::Result<GroupCountingResult> {
+        let members_len = self.members.len();
+
         let Some(leader) = self.leader_mut() else {
             return Err(io::Error::new(ErrorKind::Other, "Group has no members"));
         };
 
         use std::mem::size_of;
 
-        let header = {
-            let mut buf = [0_u8; size_of::<GroupReadFormat>()];
+        let buf = {
+            let len = size_of::<GroupReadFormat>()
+                + size_of::<GroupReadFormatValueFollowed>() * members_len;
+
+            let mut buf = unsafe { Vec::<u8>::with_len_uninit(len) };
             leader.file.read_exact(&mut buf)?;
+
+            buf
+        };
+
+        let header = {
             let ptr = buf.as_ptr() as *const GroupReadFormat;
-            unsafe { ptr.read() }
+            unsafe { ptr.read() }.clone() // clone to stack
         };
 
         let values = {
-            let mut buf =
-                vec![0_u8; size_of::<GroupReadFormatValueFollowed>() * header.member_len as usize];
-            leader
-                .file
-                .read_exact_at(&mut buf, size_of::<GroupReadFormat>() as u64)?;
+            let header_ptr = buf.as_ptr() as *const GroupReadFormat;
+            let values_ptr = unsafe { header_ptr.offset(1) as *const GroupReadFormatValueFollowed };
+            let values = unsafe { &*ptr::slice_from_raw_parts(values_ptr, members_len) };
 
-            let values_vec = unsafe {
-                Vec::from_raw_parts(
-                    buf.as_mut_ptr() as *mut GroupReadFormatValueFollowed,
-                    header.member_len as usize,
-                    header.member_len as usize,
-                )
-            };
-
-            values_vec
-                .into_iter()
+            values
+                .iter()
                 .map(|it| {
                     (
                         it.event_id,
