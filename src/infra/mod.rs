ef8490e3dd00a1a560fa5194d1aac7aa783e8022
Thaumy <thaumy@outlook.com>
2023-11-22 09:52:33 +0800

    add VecExt

--- src/infra/mod.rs
+++ src/infra/mod.rs
@@ -1,2 +1,5 @@
 pub mod option;
 pub mod result;
+pub mod vec;
+
+// TODO pub use;
---
+++ src/infra/vec.rs
@@ -0,0 +1,23 @@
+pub trait VecExt<T> {
+    unsafe fn with_len_uninit(len: usize) -> Vec<T>;
+}
+
+impl<T> VecExt<T> for Vec<T> {
+    unsafe fn with_len_uninit(len: usize) -> Vec<T> {
+        let mut vec = Vec::with_capacity(len);
+        #[allow(clippy::uninit_vec)]
+        unsafe { vec.set_len(len) };
+        vec
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::infra::vec::VecExt;
+
+    #[test]
+    fn test_with_len_uninit() {
+        let vec = unsafe { Vec::<u8>::with_len_uninit(114514) };
+        assert_eq!(vec.len(), 114514);
+    }
+}
