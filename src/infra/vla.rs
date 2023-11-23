8a4876ed5ff2aa8656f62a964c7052f3e05d0a62
Thaumy <thaumy@outlook.com>
2023-11-23 15:11:24 +0800

    impl Vla with different len size

--- src/infra/vla.rs
+++ src/infra/vla.rs
@@ -4,17 +4,79 @@ use std::slice;
 
 #[derive(Debug)]
 #[repr(C)]
-pub struct Vla<T> {
-    len: u64,
+pub struct Vla<L, T> {
+    len: L,
     pd: PhantomData<T>,
 }
 
-impl<T> Vla<T> {
-    pub unsafe fn from_ptr<P>(ptr: &P) -> &Vla<T> {
-        let ptr = ptr as *const _ as *const Vla<T>;
+impl<L, T> Vla<L, T> {
+    pub unsafe fn from_ptr<P>(ptr: &P) -> &Vla<L, T> {
+        let ptr = ptr as *const _ as *const Vla<L, T>;
         &*ptr
     }
+}
 
+impl<T> Vla<u8, T> {
+    pub fn len(&self) -> usize {
+        self.len as _
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        let len_ptr = self as *const _ as *const u8;
+        let head_ptr = unsafe { len_ptr.offset(1) } as *const T;
+        unsafe { slice::from_raw_parts(head_ptr, self.len as _) }
+    }
+}
+
+impl<T> Vla<u16, T> {
+    pub fn len(&self) -> usize {
+        self.len as _
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        let len_ptr = self as *const _ as *const u16;
+        let head_ptr = unsafe { len_ptr.offset(1) } as *const T;
+        unsafe { slice::from_raw_parts(head_ptr, self.len as _) }
+    }
+}
+
+impl<T> Vla<u32, T> {
+    pub fn len(&self) -> usize {
+        self.len as _
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        let len_ptr = self as *const _ as *const u32;
+        let head_ptr = unsafe { len_ptr.offset(1) } as *const T;
+        unsafe { slice::from_raw_parts(head_ptr, self.len as _) }
+    }
+}
+
+impl<T> Vla<u64, T> {
+    pub fn len(&self) -> usize {
+        self.len as _
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        let len_ptr = self as *const _ as *const u64;
+        let head_ptr = unsafe { len_ptr.offset(1) } as *const T;
+        unsafe { slice::from_raw_parts(head_ptr, self.len as _) }
+    }
+}
+
+impl<T> Vla<u128, T> {
+    pub fn len(&self) -> usize {
+        self.len as _
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        let len_ptr = self as *const _ as *const u128;
+        let head_ptr = unsafe { len_ptr.offset(1) } as *const T;
+        unsafe { slice::from_raw_parts(head_ptr, self.len as _) }
+    }
+}
+
+impl<T> Vla<usize, T> {
     pub fn len(&self) -> usize {
         self.len as _
     }
