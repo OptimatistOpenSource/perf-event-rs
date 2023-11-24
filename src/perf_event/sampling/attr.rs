737a1f82cea22ac195246eb9c09311239a25d928
Thaumy <thaumy@outlook.com>
2023-11-24 09:32:26 +0800

    tweak sampling attr new

--- src/perf_event/sampling/attr.rs
+++ src/perf_event/sampling/attr.rs
@@ -1,19 +1,33 @@
+use crate::counting::Event;
+use crate::EventScope;
 use crate::perf_event::RawAttr;
+
+pub enum OverflowBy {
+    Period(u64),
+    Freq(u64),
+}
 
 pub struct Attr {
     raw_attr: RawAttr,
 }
 
 impl Attr {
-    // TODO: more options needed
-    pub fn new() -> Self {
+    // TODO: more options are needed
+    pub fn new(
+        event: impl Into<Event>,
+        scopes: impl IntoIterator<Item = EventScope>,
+        overflow_by: OverflowBy
+    ) -> Self {
         use crate::syscall::bindings::*;
 
         let mut raw_attr = RawAttr {
             type_: 0, // not use in sampling mode
             size: std::mem::size_of::<RawAttr>() as libc::__u32,
             config: 0, // not use in sampling mode
-            __bindgen_anon_1: perf_event_attr__bindgen_ty_1::default(), // TODO
+            __bindgen_anon_1: match overflow_by {
+                OverflowBy::Freq(f) => perf_event_attr__bindgen_ty_1 { sample_freq: f },
+                OverflowBy::Period(p) => perf_event_attr__bindgen_ty_1 { sample_period: p },
+            },
             sample_type: {
                 #[allow(unused_mut)]
                 #[allow(clippy::identity_op)] // for readable
@@ -29,14 +43,14 @@ impl Attr {
                     | perf_event_sample_format_PERF_SAMPLE_PERIOD
                     | perf_event_sample_format_PERF_SAMPLE_STREAM_ID
                     | perf_event_sample_format_PERF_SAMPLE_RAW
-                    | perf_event_sample_format_PERF_SAMPLE_BRANCH_STACK// TODO: Not all hardware supports this feature.
-                    | perf_event_sample_format_PERF_SAMPLE_REGS_USER
-                    | perf_event_sample_format_PERF_SAMPLE_STACK_USER
+                    //| perf_event_sample_format_PERF_SAMPLE_BRANCH_STACK // TODO: Not all hardware supports this feature
+                    //| perf_event_sample_format_PERF_SAMPLE_REGS_USER // TODO
+                    //| perf_event_sample_format_PERF_SAMPLE_STACK_USER // TODO
                     | perf_event_sample_format_PERF_SAMPLE_WEIGHT
                     | perf_event_sample_format_PERF_SAMPLE_DATA_SRC
                     | perf_event_sample_format_PERF_SAMPLE_IDENTIFIER
                     | perf_event_sample_format_PERF_SAMPLE_TRANSACTION
-                    | perf_event_sample_format_PERF_SAMPLE_REGS_INTR
+                    //| perf_event_sample_format_PERF_SAMPLE_REGS_INTR // TODO
                     | perf_event_sample_format_PERF_SAMPLE_PHYS_ADDR;
 
                 #[cfg(feature = "kernel-5.7")]
@@ -55,20 +69,35 @@ impl Attr {
 
                 sample_type
             } as _, // TODO
-            read_format: 0, // TODO: consider PERF_FORMAT_LOST and PERF_SAMPLE_READ
+            read_format: {
+                #[allow(unused_mut)]
+                #[allow(clippy::identity_op)] // for readable
+                let mut read_format = 0
+                    | perf_event_read_format_PERF_FORMAT_TOTAL_TIME_ENABLED
+                    | perf_event_read_format_PERF_FORMAT_TOTAL_TIME_RUNNING
+                    | perf_event_read_format_PERF_FORMAT_ID
+                    | perf_event_read_format_PERF_FORMAT_GROUP;
+
+                #[cfg(feature = "kernel-6.0")]
+                {
+                    read_format |= perf_event_read_format_PERF_FORMAT_LOST;
+                }
+
+                read_format
+            } as _,
             _bitfield_align_1: [],
             _bitfield_1: __BindgenBitfieldUnit::new([0u8; 8usize]), // set latter via raw_attr.set_...
             __bindgen_anon_2: perf_event_attr__bindgen_ty_2::default(), // TODO
             bp_type: 0,                                             // not use in sampling mode
             __bindgen_anon_3: perf_event_attr__bindgen_ty_3::default(), // ditto
             __bindgen_anon_4: perf_event_attr__bindgen_ty_4::default(), // ditto
-            branch_sample_type: 0,                                  // TODO
-            sample_regs_user: 0,                                    // TODO
-            sample_stack_user: 0,                                   // TODO
-            clockid: 0,                                             // TODO
-            sample_regs_intr: 0,                                    // TODO
-            aux_watermark: 0,                                       // TODO
-            sample_max_stack: 0,                                    // TODO
+            branch_sample_type: 0, // TODO: Not all hardware supports this feature
+            sample_regs_user: 0,   // TODO
+            sample_stack_user: 0,  // TODO
+            clockid: 0,            // TODO
+            sample_regs_intr: 0,   // TODO
+            aux_watermark: 0,      // TODO
+            sample_max_stack: 0,   // TODO
             __reserved_2: 0,
             #[cfg(feature = "kernel-5.5")]
             aux_sample_size: 0, // TODO
@@ -91,7 +120,10 @@ impl Attr {
 
         raw_attr.set_mmap(0); // TODO
         raw_attr.set_comm(0); // not use in sampling mode
-        raw_attr.set_freq(0); // TODO
+        match overflow_by {
+            OverflowBy::Freq(_) => raw_attr.set_freq(1),
+            _ => raw_attr.set_freq(0),
+        }
         raw_attr.set_inherit_stat(0); // not use in sampling mode
         raw_attr.set_enable_on_exec(0); // ditto
         raw_attr.set_task(0); // TODO
@@ -109,7 +141,7 @@ impl Attr {
         raw_attr.set_comm_exec(0); // not use in counting mode
         raw_attr.set_use_clockid(0); // TODO
         raw_attr.set_context_switch(0); // TODO
-        raw_attr.set_write_backward(0); // TODO
+        raw_attr.set_write_backward(0);
         raw_attr.set_namespaces(0); // TODO
         raw_attr.set_ksymbol(0); // TODO
         raw_attr.set_bpf_event(0); // TODO
@@ -127,6 +159,37 @@ impl Attr {
         raw_attr.set_remove_on_exec(0); // ditto
         #[cfg(feature = "kernel-5.13")]
         raw_attr.set_sigtrap(0); // TODO
+
+        use EventScope::*;
+        scopes.into_iter().for_each(|scope| match scope {
+            User => raw_attr.set_exclude_user(0),
+            Kernel => raw_attr.set_exclude_kernel(0),
+            Hv => raw_attr.set_exclude_hv(0),
+            Idle => raw_attr.set_exclude_idle(0),
+            Host => raw_attr.set_exclude_host(0),
+            Guest => raw_attr.set_exclude_guest(0),
+            CallchainKernel => raw_attr.set_exclude_callchain_kernel(0),
+            CallchainUser => raw_attr.set_exclude_callchain_user(0),
+        });
+
+        match event.into() {
+            Event::Hw(ev) if ev.is_cache_event() => {
+                raw_attr.type_ = perf_type_id_PERF_TYPE_HW_CACHE;
+                raw_attr.config = ev.into_u64();
+            }
+            Event::Hw(ev) => {
+                raw_attr.type_ = perf_type_id_PERF_TYPE_HARDWARE;
+                raw_attr.config = ev.into_u64();
+            }
+            Event::Sw(ev) => {
+                raw_attr.type_ = perf_type_id_PERF_TYPE_SOFTWARE;
+                raw_attr.config = ev.into_u64();
+            }
+            Event::Raw(ev) => {
+                raw_attr.type_ = perf_type_id_PERF_TYPE_RAW;
+                raw_attr.config = ev.into_u64();
+            }
+        }
 
         Self { raw_attr }
     }
