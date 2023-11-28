d84b0d151b701beacfbde577e5f9dd1ee69929e4
Thaumy <thaumy@outlook.com>
2023-11-28 10:54:56 +0800

    mark follow_mem_ptr as unsafe

--- src/infra/slice.rs
+++ src/infra/slice.rs
@@ -1,9 +1,9 @@
 pub trait SliceExt<T> {
-    fn follow_mem_ptr(&self) -> *const T;
+    unsafe fn follow_mem_ptr(&self) -> *const T;
 }
 
 impl<T> SliceExt<T> for &[T] {
-    fn follow_mem_ptr(&self) -> *const T {
+    unsafe fn follow_mem_ptr(&self) -> *const T {
         let offset = self.len() as isize;
         unsafe { self.as_ptr().offset(offset) }
     }
