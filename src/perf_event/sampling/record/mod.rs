dd35a5a9962f1735edd507564b3ceccce7c37490
Thaumy <thaumy@outlook.com>
2023-11-29 10:06:31 +0800

    rm impls of perf_event_header

--- src/perf_event/sampling/record/mod.rs
+++ src/perf_event/sampling/record/mod.rs
@@ -1,4 +1,3 @@
-use crate::syscall::bindings::perf_event_header;
 /*
 pub mod aux;
 pub mod aux_output_hw_id;
@@ -24,7 +23,7 @@ pub mod text_poke;
 pub mod throttle;
 pub mod unthrottle;
 */
-#[derive(Debug)]
+/*#[derive(Debug)]
 pub(crate) enum RecordBody {
     /*
     Mmap(*const mmap::Body),
@@ -52,8 +51,8 @@ pub(crate) enum RecordBody {
     AuxOutputHwId(*const aux_output_hw_id::Body), // TODO: missing docs in manual
     */
 }
-
-#[allow(non_camel_case_types)]
+*/
+/*#[allow(non_camel_case_types)]
 pub struct sample_id {
     pid: u32,
     tid: u32,
@@ -64,55 +63,4 @@ pub struct sample_id {
     res: u32,
     id2: u64,
 }
-
-impl perf_event_header {
-    fn follow_mem_ptr(&self) -> *const perf_event_header {
-        let ptr = self as *const perf_event_header;
-        unsafe { ptr.offset(1) }
-    }
-
-    pub(crate) fn body(&self) -> RecordBody {
-        use crate::syscall::bindings::*;
-        use RecordBody::*;
-        let ptr = self.follow_mem_ptr();
-
-        macro_rules! match_enum {
-            ($(($perf_event_type:ident,$enum_type:expr,$ptr_type:ty)),+ $(,)?) => {
-                match self.type_ {
-                    $($perf_event_type => {
-                        $enum_type(ptr as *const $ptr_type)
-                    })+
-                    _ => unreachable!()
-                }
-            };
-        }
-
-        match_enum! {
-            /*
-            (perf_event_type_PERF_RECORD_MMAP,Mmap,mmap::Body),
-            (perf_event_type_PERF_RECORD_LOST,Lost,lost::Body),
-            (perf_event_type_PERF_RECORD_COMM,Comm,comm::Body),
-            (perf_event_type_PERF_RECORD_EXIT,Exit,exit::Body),
-            (perf_event_type_PERF_RECORD_THROTTLE,Throttle,throttle::Body),
-            (perf_event_type_PERF_RECORD_UNTHROTTLE,Unthrottle,unthrottle::Body),
-            (perf_event_type_PERF_RECORD_FORK,Fork,fork::Body),
-            (perf_event_type_PERF_RECORD_READ,Read,read::Body),
-            */
-            (perf_event_type_PERF_RECORD_SAMPLE,Sample,sample::raw::Body),
-            /*
-            (perf_event_type_PERF_RECORD_MMAP2,Mmap2,mmap2::Body),
-            (perf_event_type_PERF_RECORD_AUX,Aux,aux::Body),
-            (perf_event_type_PERF_RECORD_ITRACE_START,ItraceStart,intrace_start::Body),
-            (perf_event_type_PERF_RECORD_LOST_SAMPLES,LostSamples,lost_samples::Body),
-            (perf_event_type_PERF_RECORD_SWITCH,Switch,switch::Body),
-            (perf_event_type_PERF_RECORD_SWITCH_CPU_WIDE,SwitchCpuWide,switch_cpu_wide::Body),
-            (perf_event_type_PERF_RECORD_NAMESPACES,Namespaces,namespaces::Body),
-            (perf_event_type_PERF_RECORD_KSYMBOL,Ksymbol,ksymbol::Body),
-            (perf_event_type_PERF_RECORD_BPF_EVENT,BpfEvent,bpf_event::Body),
-            (perf_event_type_PERF_RECORD_CGROUP,Cgroup,cgroup::Body),
-            (perf_event_type_PERF_RECORD_TEXT_POKE,TextPoke,text_poke::Body),
-            (perf_event_type_PERF_RECORD_AUX_OUTPUT_HW_ID,AuxOutputHwId,aux_output_hw_id::Body),
-            */
-        }
-    }
-}
+*/
