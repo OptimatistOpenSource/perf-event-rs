a550b413779802798a1196856fbef61a6dac6411
Thaumy <thaumy@outlook.com>
2023-11-30 11:56:27 +0800

    add pause/resume test for sampling

--- src/perf_event/sampling/tests/hw/cpu_cycles.rs
+++ src/perf_event/sampling/tests/hw/cpu_cycles.rs
@@ -93,3 +93,35 @@ fn test_enable_disable() {
     workload();
     assert!(sampling.next_sample().is_some());
 }
+
+#[test]
+fn test_pause_resume() {
+    let builder = Builder::new().calling_process().any_cpu();
+    let attr = {
+        let event = HwEvent::CpuCycles;
+        let scopes = [EventScope::User, EventScope::Host];
+        let overflow_by = OverflowBy::Period(1000);
+        Attr::new(event, scopes, overflow_by, [])
+    };
+    let mut sampling = builder.build_sampling(&attr).unwrap();
+
+    assert!(sampling.next_sample().is_none());
+    sampling.enable().unwrap();
+    workload();
+    sampling.pause().unwrap();
+
+    {
+        let mut sample_count = 0_usize;
+        for _ in &mut sampling {
+            sample_count += 1;
+        }
+        assert!(sample_count > 0);
+    }
+
+    workload();
+    assert!(sampling.next_sample().is_none());
+
+    sampling.resume().unwrap();
+    workload();
+    assert!(sampling.next_sample().is_some());
+}
