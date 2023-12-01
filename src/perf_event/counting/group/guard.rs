a925c7821f07b0ca3b150952aab9bdfa4764cd9f
Thaumy <thaumy@outlook.com>
2023-12-01 09:30:29 +0800

    refactor CountingGroup

--- src/perf_event/counting/group/guard.rs
+++ src/perf_event/counting/group/guard.rs
@@ -1,9 +1,25 @@
+use crate::counting::{Counting, CountingResult};
+use std::io;
+use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
+
 pub struct CountingGuard {
-    pub event_id: u64,
+    pub counting: Arc<RwLock<Counting>>,
 }
 
 impl CountingGuard {
-    pub(crate) const fn new(event_id: u64) -> Self {
-        Self { event_id }
+    pub(crate) fn new(counting: Arc<RwLock<Counting>>) -> Self {
+        Self { counting }
+    }
+
+    pub(crate) fn as_counting(&self) -> RwLockReadGuard<'_, Counting> {
+        self.counting.read().unwrap()
+    }
+
+    pub(crate) fn as_counting_mut(&self) -> RwLockWriteGuard<'_, Counting> {
+        self.counting.write().unwrap()
+    }
+
+    pub fn result(&mut self) -> io::Result<CountingResult> {
+        self.as_counting_mut().result()
     }
 }
--- src/perf_event/counting/group/mod.rs
+++ src/perf_event/counting/group/mod.rs
@@ -3,8 +3,8 @@ mod result;
 
 use crate::counting::group::guard::CountingGuard;
 use crate::counting::{Attr, Counting};
-use crate::infra::VecExt;
 use crate::infra::WrapResult;
+use crate::infra::{VecExt, WrapOption};
 use crate::syscall;
 use crate::syscall::bindings::{read_format_body, read_format_header};
 use crate::syscall::ioctl_wrapped;
@@ -12,6 +12,7 @@ use libc::pid_t;
 pub use result::*;
 use std::io::{ErrorKind, Read};
 use std::os::fd::AsRawFd;
+use std::sync::{Arc,  RwLock, RwLockReadGuard, RwLockWriteGuard};
 use std::{io, slice};
 
 pub use guard::*;
@@ -20,7 +21,8 @@ pub use result::*;
 pub struct CountingGroup {
     pid: pid_t,
     cpu: i32,
-    members: Vec<Counting>, // members[0] is the group leader, if it exists.
+    leader: Option<Arc<RwLock<Counting>>>,
+    members_len: usize,
 }
 
 impl CountingGroup {
@@ -28,44 +30,51 @@ impl CountingGroup {
         Self {
             pid,
             cpu,
-            members: vec![],
+            leader: None,
+            members_len: 0,
         }
     }
 
-    fn leader(&self) -> Option<&Counting> {
-        self.members.get(0)
+    fn leader(&self) -> Option<RwLockReadGuard<'_, Counting>> {
+        self.leader.as_ref().map(|rw| rw.read().unwrap())
     }
 
-    fn leader_mut(&mut self) -> Option<&mut Counting> {
-        self.members.get_mut(0)
+    fn leader_mut(&self) -> Option<RwLockWriteGuard<'_, Counting>> {
+        self.leader.as_ref().map(|rw| rw.write().unwrap())
     }
 
     pub fn add_member(&mut self, attr: &Attr) -> io::Result<CountingGuard> {
-        let member = match self.leader() {
-            None => unsafe { Counting::new(attr, self.pid, self.cpu, -1, 0) },
-            Some(leader) => {
-                let group_fd = leader.file.as_raw_fd();
-                unsafe { Counting::new(attr, self.pid, self.cpu, group_fd, 0) }
+        let member = match &self.leader {
+            None => {
+                let counting = Arc::new(RwLock::new(unsafe {
+                    Counting::new(attr, self.pid, self.cpu, -1, 0)
+                }?));
+                self.leader = counting.clone().wrap_some();
+                counting
             }
-        }?;
+            Some(leader) => {
+                let group_fd = leader.read().unwrap().file.as_raw_fd();
+                Arc::new(RwLock::new(unsafe {
+                    Counting::new(attr, self.pid, self.cpu, group_fd, 0)
+                }?))
+            }
+        };
 
-        let event_id = member.event_id()?;
-        self.members.push(member);
+        self.members_len += 1;
 
-        CountingGuard::new(event_id).wrap_ok()
+        CountingGuard::new(member).wrap_ok()
     }
 
     pub fn result(&mut self) -> io::Result<CountingGroupResult> {
-        let members_len = self.members.len();
-
-        let Some(leader) = self.leader_mut() else {
+        let Some(mut leader) = self.leader_mut() else {
             return Err(io::Error::new(ErrorKind::Other, "Group has no members"));
         };
 
         use std::mem::size_of;
 
         let buf = {
-            let len = size_of::<read_format_header>() + size_of::<read_format_body>() * members_len;
+            let len =
+                size_of::<read_format_header>() + size_of::<read_format_body>() * self.members_len;
 
             let mut buf = unsafe { Vec::<u8>::with_len_uninit(len) };
             leader.file.read_exact(&mut buf)?;
@@ -81,7 +90,7 @@ impl CountingGroup {
         let body = {
             let header_ptr = header as *const read_format_header;
             let values_ptr = unsafe { header_ptr.add(1) as *const read_format_body };
-            unsafe { slice::from_raw_parts(values_ptr, members_len) }
+            unsafe { slice::from_raw_parts(values_ptr, self.members_len) }
         };
 
         CountingGroupResult::from_raw(header, body).wrap_ok()
