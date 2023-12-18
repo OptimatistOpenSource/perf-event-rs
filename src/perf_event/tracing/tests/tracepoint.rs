9ecd76710e78d00d9ee33b215f0d3e14718f62d5
Thaumy <thaumy.cheng@optimatist.com>
2023-12-18 17:05:29 +0800

    add tracepoint test

---
+++ src/perf_event/tracing/tests/tracepoint.rs
@@ -0,0 +1,48 @@
+use crate::event::tracing::TracepointEvent;
+use crate::sampling::record::{Record, RecordBody};
+use crate::test::{cpu_workload, read_file};
+use crate::tracing::{Config, ExtraConfig};
+use crate::{Builder, EventScope};
+use std::str::FromStr;
+
+fn gen_builder(mmap_pages: usize) -> Builder {
+    Builder::new()
+        .calling_process()
+        .any_cpu()
+        .ring_buffer_pages(mmap_pages)
+}
+
+fn gen_cfg(id: u64) -> Config {
+    let event = TracepointEvent::new(id);
+    let scopes = EventScope::all();
+
+    let mut extra_config = ExtraConfig::default();
+    extra_config.sample_record_fields.data_raw = true;
+
+    Config::new(event, scopes, &extra_config)
+}
+
+#[test]
+fn test_basic() {
+    let builder = gen_builder(1 + 512);
+    let id = {
+        let path = "/sys/kernel/debug/tracing/events/sched/sched_switch/id";
+        let string = read_file(path).replace('\n', "");
+        u64::from_str(string.as_str()).unwrap()
+    };
+    let cfg = gen_cfg(id);
+    let tracing = builder.build_tracing(&cfg).unwrap();
+
+    tracing.enable().unwrap();
+    cpu_workload();
+    tracing.disable().unwrap();
+
+    let mut sample_count = 0;
+    for Record { body, .. } in tracing {
+        if let RecordBody::Sample(body) = body {
+            sample_count += 1;
+            assert!(body.data_raw.is_some());
+        }
+    }
+    assert!(sample_count > 0);
+}
