1e6bfbbc33ce05e1419bb20f8328dc9581be1b25
Thaumy <thaumy@outlook.com>
2024-01-17 17:22:43 +0800

    expand `event::tracing` mod to `event` mod

--- src/perf_event/event/mod.rs
+++ src/perf_event/event/mod.rs
@@ -1,17 +1,30 @@
-mod hw;
+mod breakpoint;
+mod dynamic_pmu;
+mod hardware;
 mod raw;
 mod scope;
-mod sw;
-pub mod tracing;
+mod software;
+mod tracepoint;
 
-pub use hw::*;
+use crate::infra::SizedExt;
+use crate::perf_event::RawAttr;
+use crate::syscall::bindings::*;
+use libc::c_long;
+
+pub use breakpoint::*;
+pub use dynamic_pmu::*;
+pub use hardware::*;
 pub use raw::*;
 pub use scope::*;
-pub use sw::*;
+pub use software::*;
+pub use tracepoint::*;
 
 #[derive(Clone, Debug)]
 pub enum Event {
     Hardware(HardwareEvent),
     Software(SoftwareEvent),
     Raw(RawEvent),
+    Tracepoint(TracepointEvent),
+    Breakpoint(BreakpointEvent),
+    DynamicPmu(DynamicPmuEvent),
 }
